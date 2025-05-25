e5c0976708e3106ef4fb5dfc6622461d5cbc54ed
InsecurityEntBobSmith <training@in.security>
2025-05-25 14:39:02 +0000

    Oops!

--- html/wp-config.php
+++ html/wp-config.php
@@ -24,7 +24,7 @@ define( 'DB_NAME', 'wpv2' );
 
 /** MySQL database username */
 define( 'DB_USER', 'wpuser' );
-/** Developer WordPress account username/password: insecdev / qazwsx
+
 /** MySQL database password */
 define( 'DB_PASSWORD', 'M8n&b^v5c$x3z' );
 
